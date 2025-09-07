b27636e76d64b8a3c7b64b470cf3dd313d0bffe7
Ray Hernaez <r.hernz@outlook.com>
2025-09-07 17:20:51 -0400

    Added comments to the Blank Page project

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,20 +1,31 @@
 /**
- * Function Calls
+ * The Blank Page
  * Ray Hernaez
  * 
- * Working on the Function Calls examples.
+ * An exploration of the existential angst of a novelist
+ * who must sit down at their pink desk and confront the
+ * abyss that is a blank page of paper.
+ * 
+ * The program is non-interactive to convey the inability  
+ * to get started on the project. Try typing! Just try it!
  */
 
 "use strict";
 
-
+/**
+ * Creates the canvas for our masterpiece
+ */
 function setup() {
-    // Once at the beginning of the program
+    // Creates the canvas at a standard resolution
     createCanvas(640, 480);
 }
 
+/**
+ * Draws the writer's desktop and a blank piece of paper
+ */
 function draw() {
-    // Every frame
+    // The pink desktop
     background(255, 100, 100);
+    // The blank piece of paper
     rect(200, 80, 240, 320);
 }
