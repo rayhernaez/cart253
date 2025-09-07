419bc1028fa5c32515afae08fbb4111ad96c9119
Ray Hernaez <r.hernz@outlook.com>
2025-09-07 18:05:11 -0400

    Implemented the greatest record of all time

--- topics/instructions/drawing/js/script.js
+++ topics/instructions/drawing/js/script.js
@@ -1,23 +1,45 @@
 /**
- * Drawing Module
+ * The Greatest Record of All Time
  * Ray Hernaez
  * 
- * Practicing and learning drawing functions in p5
+ * Displays the Greatest Record of All Time
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a square canvas
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Displays the record
 */
 function draw() {
+    // Grey background
+    background(150);
 
+    // The main part of the record
+    push();
+    fill(255, 0, 0);
+    stroke(255);
+    ellipse(320, 320, 480);
+    pop();
+
+    // The label on the record
+    push();
+    fill("white");
+    noStroke();
+    ellipse(320, 320, 140);
+    pop();
+
+    // The hole in the record
+    push();
+    fill("#000000");
+    noStroke();
+    ellipse(320, 320, 20);
+    pop();
 }
