b7c7dfe9f8d4284719c0256a9218781e7f4b862f
Ray Hernaez <r.hernz@outlook.com>
2025-09-07 17:03:19 -0400

    Finished simulation of writer with writer's block

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,24 +1,20 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * Ray Hernaez
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Working on the Function Calls examples.
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
 
+function setup() {
+    // Once at the beginning of the program
+    createCanvas(640, 480);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+    // Every frame
+    background(255, 100, 100);
+    rect(200, 80, 240, 320);
 }
