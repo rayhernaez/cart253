dc90a35c2e8aa0e4cc35317f03034ecb1b13fd92
Ray Hernaez <r.hernz@outlook.com>
2025-09-07 17:37:13 -0400

    Started on the Drawing module

---
+++ topics/instructions/drawing/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Drawing Module
+ * Ray Hernaez
+ * 
+ * Practicing and learning drawing functions in p5
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
