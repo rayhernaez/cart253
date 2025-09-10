575256e41c8fd721698da3013bfe7bb6a33329ef
Ray Hernaez <r.hernz@outlook.com>
2025-09-10 19:51:37 -0400

    Started the creating variable module

---
+++ topics/variables/creating-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Creating Variables
+ * Ray Hernaez
+ * 
+ * Experimenting with creating variables
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
