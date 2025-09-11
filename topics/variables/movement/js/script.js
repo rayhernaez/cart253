4c455bf85cda2610b01c55e59f0fbf3085b7acc7
Ray Hernaez <r.hernz@outlook.com>
2025-09-10 21:43:59 -0400

    Started the movement module

---
+++ topics/variables/movement/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Movement
+ * Ray Hernaez
+ * 
+ * Experimenting with movement
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
