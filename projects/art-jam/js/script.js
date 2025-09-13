790356459d2ca5851fec6fe55bf2472cfcf087c3
Ray Hernaez <r.hernz@outlook.com>
2025-09-13 00:02:28 -0400

    Started drawing the head
    - I drew the shape of the head first which looked like an egg
    - Then, I drew the two eyes; I learned how to use arc shapes for the eyeballs and added "CHORD" to stroke the arc shape
    - Added nose and mouth as simple lines (might redo the mouth later)
    - todo: I need to figure out the hair; add neck, shoulders, maybe torso and finish up the body

--- projects/art-jam/js/script.js
+++ projects/art-jam/js/script.js
@@ -11,7 +11,7 @@
  * Creates the canvas
 */
 function setup() {
-
+    createCanvas(600, 600);
 }
 
 
@@ -19,5 +19,44 @@ function setup() {
  * Draws my self-portrait art
 */
 function draw() {
+    background("#e5e5e5ff");
 
+    // Draw Head
+    push();
+    noStroke();
+    fill("#d8b59fff");
+    ellipse(300, 250, 200, 240);
+    pop();
+
+    // Draw Eyes
+
+    // Eyeballs
+    push();
+    // noStroke();
+    fill("#ffffffff");
+    arc(260, 260, 50, 40, 0, -PI, CHORD);
+    arc(340, 260, 50, 40, 0, -PI, CHORD);
+    pop();
+
+    // Pupils
+    push();
+    noStroke();
+    fill("#5d3f2eff");
+    ellipse(260, 270, 15);
+    ellipse(340, 270, 15);
+    pop();
+
+    // Draw Nose
+    push();
+    stroke("#5d3f2eff");
+    line(305, 290, 310, 310);
+    line(310, 310, 305, 315);
+    pop();
+
+    // Draw Mouth
+    push();
+    stroke("#d48587ff");
+    strokeWeight(5);
+    line(290, 330, 310, 330);
+    pop();
 }
