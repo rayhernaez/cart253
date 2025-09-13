e53d620a2a54e97dbb205427457ffdfbf7a53bdb
Ray Hernaez <r.hernz@outlook.com>
2025-09-12 21:23:38 -0400

    Started the art jam
    - I didn't forget to place a title in the html so tab is not nameless
    - I also edited the top comments by adding the title, my name, a short description of the project (though I might change it later), and basic descriptions of the first two functions
    - todo: I need to plan the visuals and sketch the base artwork 😥

---
+++ projects/art-jam/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Art Jam
+ * Ray Hernaez
+ * 
+ * My Self Portrait using p5
+ */
+
+"use strict";
+
+/**
+ * Creates the canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * Draws my self-portrait art
+*/
+function draw() {
+
+}
