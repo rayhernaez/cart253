e1070ee64096b8426a5bdd8632ebb58174a54d39
Ray Hernaez <r.hernz@outlook.com>
2025-09-23 19:46:15 -0400

    Added keyboard controls for the colour

--- topics/events/keyboard-events/js/script.js
+++ topics/events/keyboard-events/js/script.js
@@ -21,6 +21,10 @@ const ball = {
         white: "#ffffff",
         red: "#ff0000",
         blue: "#0000ff"
+    },
+    keys: {
+        redKey: 82, // R
+        blueKey: 66 // B
     }
 }
 
@@ -43,4 +47,19 @@ function draw() {
     fill(ball.fill);
     ellipse(ball.x, ball.y, ball.size);
     pop();
+}
+
+function keyPressed(event) {
+    if (event.keyCode === ball.keys.redKey) {
+        ball.fill = ball.fills.red;
+    }
+    else if (event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.blue;
+    }
+}
+
+function keyReleased(event) {
+    if (event.keyCode === ball.keys.redKey || event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.white;
+    }
 }
