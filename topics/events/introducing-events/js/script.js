89c88412706ed8eef3704886ce76bcb971a4820d
Ray Hernaez <r.hernz@outlook.com>
2025-09-23 19:02:57 -0400

    Started introducing events module

---
+++ topics/events/introducing-events/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing Events
+ * Ray Hernaez
+ * 
+ * Taking a look at how events work in JavaScript and p5
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
