0071a199cfb98f4d670408c7da1c60c7e5120f03
Ray Hernaez <r.hernz@outlook.com>
2025-10-21 20:20:21 -0400

    Finished Speech! Speech! project

--- topics/arrays/speech-speech/js/script.js
+++ topics/arrays/speech-speech/js/script.js
@@ -7,11 +7,16 @@
 
 "use strict";
 
+// The speech itself
+const speech = ["Veni.", "Vidi.", "Vici.", "Sensi malum."];
+// Which sentence in the speech to display
+let speechIndex = 0;
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(600, 100);
 }
 
 
@@ -19,5 +24,25 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    // Get the current line of the speech
+    let currentLine = speech[speechIndex];
+
+    // DIsplay the line
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentLine, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    speechIndex++;
+    // handle end of speech
+    if (speechIndex >= speech.length) {
+        // startover
+        speechIndex = 0;
+    }
 }
