63f915c0c17dbcb4d16a7873f5c85800296a7433
Ray Hernaez <r.hernz@outlook.com>
2025-10-21 20:41:35 -0400

    Finished Flies Flies project

--- topics/arrays/flies-flies/js/script.js
+++ topics/arrays/flies-flies/js/script.js
@@ -14,17 +14,20 @@ let flies = [
     {
         x: 100,
         y: 125,
-        size: 10
+        size: 10,
+        buzziness: 4
     },
     {
         x: 160,
         y: 170,
-        size: 14
+        size: 14,
+        buzziness: 2
     },
     {
         x: 180,
         y: 50,
-        size: 5
+        size: 5,
+        buzziness: 3
     }
 ];
 
@@ -44,8 +47,15 @@ function draw() {
 
     // Display each fly in the array
     for (let fly of flies) {
+        moveFly(fly);
         drawFly(fly);
     }
+}
+
+// Moves fly randomly - buzziness
+function moveFly(fly) {
+    fly.x += random(-fly.buzziness, fly.buzziness);
+    fly.y += random(-fly.buzziness, fly.buzziness);
 }
 
 /**
