785264615af476f7ca19a7fc1721aaf310c8966b
Ray Hernaez <r.hernz@outlook.com>
2025-10-28 19:37:42 -0400

    Finished Danger Zone project

--- topics/loops/the-danger-zone/js/script.js
+++ topics/loops/the-danger-zone/js/script.js
@@ -59,7 +59,14 @@ function placeHero() {
     // Choose a random spot for the hero
     hero.x = random(0, width);
     hero.y = random(0, height);
-    // This COULD BE IN THE DANGER ZONE
+
+    let d = dist(hero.x, hero.y, dangerZone.x, dangerZone.y);
+
+    while (d < hero.size / 2 + dangerZone.size / 2) {
+        hero.x = random(0, width);
+        hero.y = random(0, height);
+        d = dist(hero.x, hero.y, dangerZone.x, dangerZone.y);
+    }
 }
 
 /**
