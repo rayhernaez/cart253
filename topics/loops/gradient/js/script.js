b428c583dc62c21001810c12b59b38f7c542ed47
Ray Hernaez <r.hernz@outlook.com>
2025-10-28 20:34:52 -0400

    Finished Gradeint project

--- topics/loops/gradient/js/script.js
+++ topics/loops/gradient/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Gradient
+ * Ray Hernaez
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(600, 300);
 }
 
 
@@ -20,5 +20,14 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    for (let x = 0; x <= width; x++) {
+        const shade = map(x, 0, width, 0, 255);
+
+        push();
+        stroke(shade);
+        line(x, 0, x, height);
+        pop();
+    }
 }
