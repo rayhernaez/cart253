cc0cc31b6eefccb78c6bfb28441aa83dce42be2c
Ray Hernaez <r.hernz@outlook.com>
2025-10-28 20:28:16 -0400

    Finished Starfield

--- topics/loops/star-field/js/script.js
+++ topics/loops/star-field/js/script.js
@@ -8,11 +8,13 @@
 
 "use strict";
 
+const numStars = 100;
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
 
@@ -20,5 +22,25 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    // Freezes random every frame
+    // You can change 1 to any number
+    randomSeed(1);
+
+    for (let i = 0; i < numStars; i++) {
+        drawStar();
+    }
+}
+
+function drawStar() {
+    const x = random(0, width);
+    const y = random(0, height);
+    const diameter = random(2, 5);
+
+    push();
+    fill(255);
+    noStroke();
+    ellipse(x, y, diameter);
+    pop();
 }
