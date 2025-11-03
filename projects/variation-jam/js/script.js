3e07f9e43da850db4ae6e0e624966dafccce3ffc
Ray Hernaez <r.hernz@outlook.com>
2025-11-03 13:07:27 -0500

    Started the variation jam
    - This is my initial push for this project
    - I added a html title and updated the top-level comments
    - todo: plan 3 variations of a game/narrative concept

---
+++ projects/variation-jam/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Variation Jam
+ * Ray Hernaez
+ * 
+ * // description
+ * 
+ * // instructions
+ * 
+ * Made with p5
+ * https://p5js.org/
+ */
+
+"use strict";
+
+/**
+ * Creates the canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * Draws the current screen
+*/
+function draw() {
+
+}
