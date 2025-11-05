a06e02d5682f11f29022886fec8b9b99b1a1e5e3
Ray Hernaez <r.hernz@outlook.com>
2025-11-04 19:31:10 -0500

    Finished Tarot project

--- topics/data/tarot/js/script.js
+++ topics/data/tarot/js/script.js
@@ -7,11 +7,20 @@
 
 "use strict";
 
+// Our tarot data
+let tarot = undefined;
+
+let fortune = "Click to show a fortune.";
+
+function preload() {
+    tarot = loadJSON("assets/data/tarot_interpretations.json");
+}
+
 /**
  * tbd.
 */
 function setup() {
-
+    createCanvas(800, 400);
 }
 
 
@@ -19,5 +28,18 @@ function setup() {
  * tbd.
 */
 function draw() {
+    background(0);
 
+    // Display info
+    push();
+    textSize(16);
+    fill("yellow");
+    textAlign(CENTER, CENTER);
+    text(fortune, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    const card = random(tarot.tarot_interpretations);
+    fortune = random(card.fortune_telling);
 }
